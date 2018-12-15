cfc2cfe4865a366ce95e84494128d7ff6aaa9867
Pavel Kosilo <kodosgames@gmail.com>
2018-12-15 19:38:51 +0300

    saving state?

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 import * as Cartographer from '../../Cartographer/dist/cartographer.js';
 
 @Component({
@@ -7,22 +7,47 @@ import * as Cartographer from '../../Cartographer/dist/cartographer.js';
   styleUrls: ['./map.component.sass']
 })
 
-export class MapComponent implements OnInit {
+export class MapComponent implements OnInit, OnDestroy {
+
+  map;
 
   constructor() { }
 
   ngOnInit() {
-    const map = Cartographer.map('map', {
+    var x = 53.902262;
+    var y = 27.561840;
+    var z = 7;
+
+    if(localStorage.getItem("app-map-coords-x")) {
+      x = parseFloat(localStorage.getItem("app-map-coords-x"));
+    }
+
+    if(localStorage.getItem("app-map-coords-y")) {
+      y = parseFloat(localStorage.getItem("app-map-coords-y"));
+    }
+
+    if(localStorage.getItem("app-map-coords-z")) {
+      z = parseFloat(localStorage.getItem("app-map-coords-z"));
+    }
+
+    this.map = Cartographer.map('map', {
       zoomControl: false
-    }).setView([53.902262, 27.561840], 7);
+    }).setView([x, y], z);
 
     Cartographer.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
+    }).addTo(this.map);
 
     Cartographer.control.zoom({
       position:'bottomright'
-    }).addTo(map);
+    }).addTo(this.map);
+  }
+
+  ngOnDestroy(): void {
+    const coords = this.map.getCenter();
+    localStorage.setItem("app-map-coords-x", coords.x);
+    localStorage.setItem("app-map-coords-y", coords.y);
+    localStorage.setItem("app-map-coords-z", coords.z);
   }
 
 }
