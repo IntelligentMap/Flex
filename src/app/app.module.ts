1644fca9bf3817b32f0bba28a2e879e23d55032b
Pavel Kosilo <kodosgames@gmail.com>
2018-12-16 19:16:34 +0300

    adding search UI. SEARCH IS NOT WORKING!!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,19 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { BlogComponent } from './blog/blog.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageHeaderComponent,
     MapComponent,
     PageNotFoundComponent,
     BlogComponent,
-    SearchComponent
+    PageHeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/page-header/page-header.component.ts
+++ src/app/page-header/page-header.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
-  styleUrls: ['./page-header.component.sass']
+  styleUrls: ['./page-header.component.sass'],
 })
 export class PageHeaderComponent implements OnInit {
 
